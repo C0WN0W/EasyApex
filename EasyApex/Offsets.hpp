9bb32154d6758243363b0696fa8c13d000e93df7
C0WN0W <imcownow@yeah.net>
2025-09-29 16:40:56 +0800

    Update offsets

--- EasyApex/Offsets.hpp
+++ EasyApex/Offsets.hpp
@@ -1,9 +1,30 @@
 #pragma once
 #include <cstdint>
 
-const uint64_t OFF_ENTITYLIST = 0x6282D98;
-const uint64_t OFF_VIEW_MATRIX = 0x11A350;
-const uint64_t OFF_VIEW_RENDER = 0x3D3CEF8;
-const uint64_t OFF_VecAbsOrigin = 0x17C;
-const uint64_t OFF_iHealth = 0x324;
-const uint64_t OFF_iMaxHealth = 0x468;
+#define OFF const uint64_t
+
+OFF OFF_LEVELNAME = 0x1BD2D14;
+OFF OFF_ENTITYLIST = 0x6282D98;
+OFF OFF_LOCALPLAYER = 0x26DF498;
+OFF OFF_NAMELIST = 0x8CDB7F0;
+OFF OFF_VIEW_MATRIX = 0x11A350;
+OFF OFF_VIEW_RENDER = 0x3D3CEF8;
+OFF OFF_OBSERVERLIST = 0x6284DB8;
+
+OFF OFF_VecAbsOrigin = 0x17C;
+OFF OFF_iTeamNum = 0x334;
+OFF OFF_iHealth = 0x324;
+OFF OFF_iMaxHealth = 0x468;
+OFF OFF_ShieldHealth = 0x1A0;
+OFF OFF_iSignifierName = 0x470;
+OFF OFF_BleedoutState = 0x2830;
+OFF OFF_LifeState = 0x690;
+OFF OFF_DuckState = 0x2B2C;
+OFF OFF_LastVisibleTime = 0x1A54;
+OFF OFF_CBaseAnimating = 0xFD0;
+OFF OFF_DeathFieldRadiusEnd = 0xDD4;
+OFF OFF_VecAbsVelocity = 0x170;
+
+OFF OFF_NAME_INDEX = 0x38;
+OFF OFF_OBSERVER_ARRAY = 0x958;		// m_playerObserver
+OFF OFF_TIMEBASE = 0x2158;		// m_currentFramePlayer.timeBase
